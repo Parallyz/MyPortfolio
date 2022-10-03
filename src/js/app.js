585790e8bd51cc2d708dfad43781b174e7595da4
Vladyslav Verus <58305519+Parallyz@users.noreply.github.com>
2022-10-03 13:53:32 +0300

    Delete src directory

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-
-import "./modules/burger.js";
-import "./modules/rellax.js";
-import "./modules/swiper.js";
-import "./modules/intersectionObserver.js";
-
-flsFunctions.isWebp();
